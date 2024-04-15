f31f336d357bb5e95ca3762b08a9b066f92f5193
Ashley Kolodziej <likeapenguininthedark@gmail.com>
2024-04-15 15:58:16 -0400

    Show and hide sections

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,4 +5,36 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
-console.log("Hello!");
+
+const startBtn = document.querySelector('#js-start');
+
+function goToNext() {
+  const thisSection = this.closest('.rps-section');
+	let nextSection = thisSection.nextElementSibling;
+
+	if ( nextSection === null ) {
+		nextSection = document.querySelector('#rps-intro');
+	}
+
+	thisSection.classList.toggle('show');
+	nextSection.classList.toggle('show');
+}
+
+startBtn.addEventListener("click", goToNext);
+
+// Event delegation lets us do the same event for a larger area,
+// and we "delegate" - or check - if the element is a certain class
+// to know if we want to go forward.
+
+const moveSelect = document.querySelector('#js-moves');
+
+moveSelect.addEventListener("click", function(event) {
+	if (event.target.classList.contains('rps-move')) {
+		//console.log(event.target);
+		goToNext.call(event.target);
+	}
+});
+
+// Last event listener!
+const tryAgainBtn = document.querySelector('#js-tryagain');
+tryAgainBtn.addEventListener("click", goToNext);
